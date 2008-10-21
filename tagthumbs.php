66b26ca3d81135fb6119979120ce4b2e0a029565
Mark Ellis <m@rkellis.com>
2008-10-21 17:32:36 +0000

    fixed tagthumbs issue

--- tagthumbs.php
+++ tagthumbs.php
@@ -1,8 +1,8 @@
 <?php
 require_once('code/tag.php');
-require_once('code/picture.php');
+require_once('code/unified.php');
 $tag = new tag();
-$picture = new picture();
+$picture = new unified('picture');
 
 $noPage = 1;
 
@@ -12,10 +12,10 @@ $alreadydone = array();
 foreach ($res as $tagrow){
     foreach ($tagrow as $picid){
         if (!in_array($picid['picture_id'], $alreadydone)){
-            $details = $picture->getPicDetails($picid['picture_id']);
+            $details = $picture->getPageObjects(null, null, null, null, $picid['picture_id']);
             $images .="<img id='pic{$picid['picture_id']}' 
                 style='height:50px;width:50px;cursor:pointer;vertical-align:middle;margin-top:15px;' 
-                src='/thumb/{$details['picture_id']}/100/' alt={$picid['picture_id']} 
+                src='/thumb/{$details[0]['picture_id']}/100/' alt={$picid['picture_id']} 
                 onclick=\"selectImage(this.id);return false;\" />\n";
             $alreadydone[] = $picid['picture_id'];
         }
