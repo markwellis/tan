b628731a1d68a0a7559119b2fd4a75ee023a28e3
Mark Ellis <m@rkellis.com>
2009-02-28 15:04:06 +0000

    was orderd wrong

--- code/unified.php
+++ code/unified.php
@@ -528,8 +528,10 @@ if (defined('MAGIC')) {
             $page = ($page * 27) - 27;
             if ($below == 0) {
                 $oper = '>=';
+                $orderby = 'promoted';
             } else if ($below == 1){
                 $oper = '<';
+                $orderby = 'date';
             }
             if (isset($oper)) { 
                 $uid = -1;
@@ -541,7 +543,7 @@ if (defined('MAGIC')) {
                     (SELECT count(*) from comments WHERE comments.picture_id = picture_details.picture_id) as comments,
                     (SELECT count(*) from plus where plus.picture_id = picture_details.picture_id and plus.user_id=$uid) as meplus,
                     (SELECT count(*) from minus where minus.picture_id = picture_details.picture_id and minus.user_id=$uid) as meminus
-                    from picture_details HAVING plus $oper ".$this->promoted_threashold." order by date desc limit $limit;";
+                    from picture_details HAVING plus $oper ".$this->promoted_threashold." order by ($orderby) desc limit $limit;";
                 return $sql->query($query, 'array');
             }
             return -1;
