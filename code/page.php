ed7afc7f476d579f57742cc298076284f80b2c19
Mark Ellis <m@rkellis.com>
2009-03-14 20:59:25 +0000

    search

--- code/page.php
+++ code/page.php
@@ -306,8 +306,19 @@ if (defined('MAGIC')) {
 	        if ($user->isLoggedIn()){
 	            $this->output .= " | <a href='/logout/' class='menulink'>Logout</a>";
 	        }
-	        $this->output .= " </div>
-	            <div class='logoimg'/><a href='/' class='logo'></a></div>";
+	        $this->output .= '<div style="float:right">
+<form action="http://www.google.com/cse" id="cse-search-box">
+  <div>
+    <input type="hidden" name="cx" value="017135894524023845720:-eqs9gh9cxm" />
+    <input type="hidden" name="ie" value="UTF-8" />
+    <input type="text" name="q" size="31" />
+    <input type="submit" name="sa" value="Search" />
+  </div>
+</form>
+
+<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en"></script></div>
+</div><div class="logoimg"/><a href="/" class="logo"></a></div>';
+
 	        $this->output .= "</div><div id='middle'>"; 
 	    }
 	
