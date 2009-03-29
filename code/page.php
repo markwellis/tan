540c1141b2afa9b599898bb580b9e4cc65fe70ca
Mark Ellis <m@rkellis.com>
2009-03-29 17:09:38 +0000

    CSS fix ups

--- code/page.php
+++ code/page.php
@@ -191,7 +191,7 @@ if (defined('MAGIC')) {
 	            .'<meta name="keywords" content="news community comments english lulz lol social lulzhq fun jokes '
 	            .'videos pictures share sharing lol lolz funny humour humur"/> '
 	            .'<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> '
-	            .'<link rel="stylesheet" type="text/css" title="default" href="/css/default.css?2=13" /> '
+	            .'<link rel="stylesheet" type="text/css" title="default" href="/css/default.css?2=14" /> '
 	            .'<link rel="shortcut icon" href="/favicon.ico" /> '
 	            .$script
 	            .'</head> ';
