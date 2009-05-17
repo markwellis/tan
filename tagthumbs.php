1ad8fd03df38fdafc5908a72ed803a6ab6466276
Mark Ellis <m@rkellis.com>
2009-05-17 01:22:08 +0000

    fixed tag issue, already fixed in future with new submit pages via json

--- tagthumbs.php
+++ tagthumbs.php
@@ -17,10 +17,9 @@ $alreadydone = array();
 foreach ($res as $tagrow){
     foreach ($tagrow as $picid){
         if (!in_array($picid['picture_id'], $alreadydone)){
-            $details = $picture->getPageObjects(null, null, null, null, $picid['picture_id']);
             $images .="<img id='pic{$picid['picture_id']}' 
                 style='height:50px;width:50px;cursor:pointer;vertical-align:middle;margin-top:15px;' 
-                src='/thumb/{$details[0]['picture_id']}/100/' alt={$picid['picture_id']} 
+                src='/thumb/{$picid['picture_id']}/100/' alt={$picid['picture_id']} 
                 onclick=\"selectImage(this.id);return false;\" />\n";
             $alreadydone[] = $picid['picture_id'];
         }
