461e1d3d39dfafa047ca193348525bdd2720f002
Mark Ellis <m@rkellis.com>
2009-06-21 16:15:20 +0000

    changed mibbit to use https

--- chat.php
+++ chat.php
@@ -12,7 +12,7 @@ if ($uname == ''){
 
 $middle .= '<div style="text-align:center;">'
     .'<iframe width=720 height=400 scrolling=no style="border:0" '
-    .'src="http://embed.mibbit.com/?server=irc.thisaintnews.com%3A%2B6697'
+    .'src="https://widget.mibbit.com/?server=irc.thisaintnews.com%3A%2B6697'
     .'&chatOutputShowTimes=true&autoConnect=true'
     .'&channel=%23thisaintnews&settings=8a8a5ac18a22e7eecd04026233c3df93&nick='.$uname.'">'
     .'</iframe></div>';
