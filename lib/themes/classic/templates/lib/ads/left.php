d3d0e6255e34dbe4eb5d9878f7ac7e062a4ac071
Mark Ellis <m@rkellis.com>
2009-07-04 22:35:17 +0000

    #64 remove nsfw ads

--- lib/themes/classic/templates/lib/ads/left.php
+++ lib/themes/classic/templates/lib/ads/left.php
@@ -1,27 +1,3 @@
 <div id="left_ad">
-<?php if (isset($_SESSION['nsfw'])){ ?>
-    <script type="text/javascript">
-        //<![CDATA[
-        
-        var AdBrite_Title_Color = '0000FF';
-        var AdBrite_Text_Color = '000000';
-        var AdBrite_Background_Color = '808080';
-        var AdBrite_Border_Color = 'CCCCCC';
-        var AdBrite_URL_Color = '008000';
-        try{var AdBrite_Iframe=window.top!=window.self?2:1;var AdBrite_Referrer=document.referrer==''?document.location:document.referrer;AdBrite_Referrer=encodeURIComponent(AdBrite_Referrer);}catch(e){var AdBrite_Iframe='';var AdBrite_Referrer='';}
-        
-        //]]>
-    </script>
-    
-    <script type="text/javascript">
-    //<![CDATA[
-        document.write(String.fromCharCode(60,83,67,82,73,80,84));
-        document.write(' src="http://ads.adbrite.com/mb/text_group.php?sid=1159321&zs=3136305f363030&ifr='
-            +AdBrite_Iframe+'&ref='+AdBrite_Referrer+'" type="text/javascript">');
-        document.write(String.fromCharCode(60,47,83,67,82,73,80,84,62));
-    //]]>
-    </script>
-<?php } else { ?>
-    <script type='text/javascript' src="http://hosting.adjug.com/AdJugSearch/PageBuilder.aspx?ivi=V3.0+JS&amp;aid=1572&amp;slid=102791&amp;height=600&amp;width=160&amp;HTMLOP=False&amp;ShowIFrame=True"></script>
-<?php } ?>
+    <iframe width="160" height="600" name="AdSpace102791" src="http://hosting.adjug.com/AdJugSearch/PageBuilder.aspx?ivi=V3.0+IF&aid=1572&slid=102791&height=600&width=160&CacheBuster=[time_stamp]&HTMLOP=True" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no"></iframe>
 </div>
