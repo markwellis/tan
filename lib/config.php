38b64c4b1606ab997f9a3b32957df7063a5b668a
Mark Ellis <m@rkellis.com>
2009-07-04 14:21:19 +0000

    pushing 0.82.5 to live

--- lib/config.php
+++ lib/config.php
@@ -28,7 +28,7 @@ define('PROFILE_PICTURE_UPLOAD_PATH', BASE_PATH . '/sys/users/avatar');
 /**
  * Site settings
  */
-define('VERSION', '0.82.4');
+define('VERSION', '0.82.5');
 
 define('DEBUG', false);
 define('DEBUG_SQL', false);
