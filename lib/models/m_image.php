457ea41cddf534dd623b2ea5fe86c9546ea760c0
Mark Ellis <m@rkellis.com>
2009-07-04 02:46:22 +0000

    fixed image lookup version bump 0.82.4

--- lib/models/m_image.php
+++ lib/models/m_image.php
@@ -7,8 +7,8 @@ class m_image {
     }
     
     function get_by_filename($filename){
-        $query = "SELECT picture_id, title FROM picture_details WHERE filename = ?";
-        $row = $this->m_sql->query($query, 's', array(PICTURE_PATH . "/{$filename}"));
+        $query = "SELECT picture_id, title FROM picture_details WHERE filename LIKE ?";
+        $row = $this->m_sql->query($query, 's', array("%/images/pics/{$filename}"));
         return $row[0];
     }
 }
