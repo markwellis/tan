48c5cc9756815a45f2a73d52c808fd4eddbcbcdb
Mark Ellis <m@rkellis.com>
2009-07-05 00:23:13 +0000

    #62 fixed image placeholder loading before image loading causing image not to show

--- sys/js/nsfw_comments.js
+++ sys/js/nsfw_comments.js
@@ -1,29 +1,46 @@
-window.addEvent('domready', function() {
+window.addEvent('load', function() {
     $$('.comment img').each(function(el) {
         if (el.src.indexOf('/sys/js/fckeditor/editor/images/smiley/') < 0){
             var width = el.width;
             var height = el.height;
 
-            el.title = el.src;
-            el.src = '/sys/images/mouseover.jpg';
-            el.style.border = '1px solid #000';
+            first_hide(el);
 
-            el.height = height;
-            el.width = width;
-    
             el.addEvent('mouseenter', function(e){
-                var src = el.title;
-                el.title = el.src;
-                el.src = src;
-                el.style.border = '1px solid #fff';
+                show_image(this);
             });
     
             el.addEvent('mouseleave', function(e){
-                var title = el.src;
-                el.src = el.title;
-                el.title = title;
-                el.style.border = '1px solid #000';
+                hide_image(this);
             });
         }
     });
 });
+
+function first_hide(el){
+    var width = el.width;
+    var height = el.height;
+
+    el.title = el.src;
+    el.src = '/sys/images/mouseover.jpg';
+    el.style.border = '1px solid #000';
+    
+    if (width && height){
+        el.width = width;
+        el.height = height;
+    }
+}
+
+function hide_image(el){
+    var title = el.src;
+    el.src = el.title;
+    el.title = title;
+    el.style.border = '1px solid #000';
+}
+
+function show_image(el){
+    var src = el.title;
+    el.title = el.src;
+    el.src = src;
+    el.style.border = '1px solid #fff';
+}
