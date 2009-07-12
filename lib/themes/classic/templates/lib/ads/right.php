704ee6bfb5b8d038624c8bd8a903b19bb79404dd
Mark Ellis <m@rkellis.com>
2009-07-12 16:38:44 +0000

    pushin 0.84.1 to live

--- lib/themes/classic/templates/lib/ads/right.php
+++ lib/themes/classic/templates/lib/ads/right.php
@@ -1,3 +1,10 @@
 <div id="right_ad">
-    <iframe width="120" height="600" name="AdSpace101951" src="http://hosting.adjug.com/AdJugSearch/PageBuilder.aspx?ivi=V3.0+IF&aid=1572&slid=101951&height=600&width=120&CacheBuster=[time_stamp]&HTMLOP=True" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no"></iframe>
+    <script type="text/javascript"> 
+            //<![CDATA[
+                window.addEvent("domready", function() {
+                var html = '<iframe width="120" height="600" name="AdSpace101951" src="http://hosting.adjug.com/AdJugSearch/PageBuilder.aspx?ivi=V3.0+IF&aid=1572&slid=101951&height=600&width=120&CacheBuster=[time_stamp]&HTMLOP=True" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no"></iframe>';
+                    $('right_ad').set('html', html);
+                });
+            //]]>
+    </script>
 </div>
