77360b8b99a38f0e664d59deabfb969cfc831d54
Mark Ellis <m@rkellis.com>
2009-08-07 14:51:21 +0000

    changed image resizer to use new code changed image resizer to resize animated gifs better

--- lib/controllers/thumb.php
+++ lib/controllers/thumb.php
@@ -6,18 +6,13 @@
  * License is in the root directory, entitled LICENSE
  * @package package_name
  */
-
-require_once('../config.php');
-
-require_once(OLD_CODE_PATH . '/sql.php');
-require_once(MODEL_PATH . '/m_image_resize.php');
-global $sql;
-$sql = &new sql();
+require_once('../header.php');
 
 $id = (int)$_GET['id'];
 $x = (int)$_GET['x'];
     
-$newimg = &new image_resize();
+$newimg = load_model('m_image_resize');
+
 $image = $newimg->resize($id, $x);
 
 if ($image){
