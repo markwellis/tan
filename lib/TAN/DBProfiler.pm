5b099efb1d443f500ea97370d102e3ad6ec102dc
Mark Ellis <m@rkellis.com>
2009-11-01 23:23:10 +0000

    sql output wrap

--- lib/TAN/DBProfiler.pm
+++ lib/TAN/DBProfiler.pm
@@ -4,6 +4,7 @@ use base 'DBIx::Class::Storage::Statistics';
 
 use strict;
 use Time::HiRes qw(time);
+use Text::Wrap;
 
 my $start;
 
@@ -25,11 +26,12 @@ sub query_end {
     $self->{total_time} += $time;
     $self->{_queries}++;
 
-    my $error = '';
+    my $error = "\n";
     if ($time > 0.05) {
         $error .= "\n\n" . 'v='x18 . 'v SLOW v' . '=v'x18 . "\n\n";
     }
-    $error .= 'Query #' . $self->{_queries} . ': execution time = ' . $time . "\n" . $self->{full_sql_with_params} . "\n";
+    $error .= 'Query #' . $self->{_queries} . ': execution time = ' . $time . "\n" 
+        . Text::Wrap::wrap("\t","\t",$self->{'full_sql_with_params'}) . "\n\n";
     if ($time > 0.05) {
         $error .= "\n" . '^='x40 . "\n\n\n";
     }
