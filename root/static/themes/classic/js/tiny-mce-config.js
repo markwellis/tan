86dd89408d50d6c8d36662a4ea3150f424d8f46b
Mark Ellis <m@rkellis.com>
2009-12-06 14:58:58 +0000

    #83 - external config file for tiny-mce

---
+++ root/static/themes/classic/js/tiny-mce-config.js
@@ -0,0 +1,17 @@
+tiny_mce_config = {
+    theme: "advanced",
+
+    plugins: "safari,table,emotions,fullscreen,inlinepopups",
+
+    theme_advanced_buttons1 : "code,|,undo,redo,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull|,sub,sup,|,forecolor,backcolor,|,fontselect,fontsizeselect",
+    theme_advanced_buttons2 : "fullscreen,|,hr,bullist,numlist,|,link,unlink,image,emotions,|,tablecontrols,",
+    theme_advanced_buttons3 : "",
+    theme_advanced_buttons4 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true,
+
+    mode:"specific_textareas",
+    gecko_spellcheck:"1"
+};
