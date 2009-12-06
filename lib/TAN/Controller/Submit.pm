6d0b5dfc1ce5435539aca6e437c7d564e1fe2722
Mark Ellis <m@rkellis.com>
2009-12-06 02:18:09 +0000

    link validate first commit

--- lib/TAN/Controller/Submit.pm
+++ lib/TAN/Controller/Submit.pm
@@ -3,6 +3,7 @@ package TAN::Controller::Submit;
 use strict;
 use warnings;
 use parent 'Catalyst::Controller';
+use Data::Validate::URI;
 
 =head2 index
 
@@ -10,6 +11,9 @@ use parent 'Catalyst::Controller';
 my $location_reg = qr/^link|blog|picture$/;
 my $int_reg = qr/\D+/;
 
+=head2 location
+checks the location is valid
+=cut
 sub location: Chained('/') PathPart('submit') CaptureArgs(1){
     my ( $self, $c, $location ) = @_;
 
@@ -26,15 +30,109 @@ sub location: Chained('/') PathPart('submit') CaptureArgs(1){
     $c->stash->{'location'} = $location;
 }
 
+
+=head2 index
+the submit page
+=cut
 sub index : PathPart('') Chained('location') Args(0) {
     my ( $self, $c ) = @_;
 
     $c->stash->{'template'} = 'submit.tt';
 }
 
-sub post: PathPart('post') Chained('location') Args(0){
+
+=head2 validate
+validates the upload
+=cut
+sub validate: PathPart('') Chained('location') CaptureArgs(0){
     my ( $self, $c ) = @_;
 
+    my $title_min = 5;
+    my $desc_min = 5;
+    my $title_max = 100;
+    my $desc_max = 1000;
+    my $blog_min = 20;
+
+    my @error_codes = (
+        'Title cannont be blank',
+        'Description cannot be blank',
+        "Title cannot be over ${title_max} characters",
+        "Description cannot be over ${desc_max} characters",
+        "Title must be over ${title_min} characters",
+        "Description must be over ${desc_min} characters",
+        'Url is invalid',
+        'This link has already been submitted',
+        "Blog must be over ${blog_min} characters",
+    );
+
+    my $valid = 0;
+    my $title = $c->req->param('title');
+    my $description = $c->req->param('description');
+
+    if ( $title eq '' ) {
+        #blank title
+        $c->stash->{'error'} = $error_codes[0];
+
+    } elsif ( length($title) > $title_max ) {
+        #long title
+        $c->stash->{'error'} = $error_codes[2];
+
+    } elsif ( length($c->req->param('description')) > $desc_max ) {
+        #long description
+        $c->stash->{'error'} = $error_codes[3];
+
+    } elsif ( length($title) < $title_min ) {
+        #short title
+        $c->stash->{'error'} = $error_codes[4];
+
+    } else {
+        if ($c->stash->{'location'} eq 'link'){
+            if ($description eq ''){
+                #link blank description
+                $c->stash->{'error'} = $error_codes[1];
+            }
+
+            if (length($description) < $desc_min){
+                #desc too short
+                $c->stash->{'error'} = $error_codes[5];
+            }
+
+            my $valid_url = Data::Validate::URI->new();
+            my $url = $c->req->param('url');
+
+            if (!$valid_url->is_http_uri($url) && !$valid_url->is_https_uri($url)){
+                #invalid url
+                $c->stash->{'error'} = $error_codes[6];                
+            }
+
+            my $link = $c->model('MySQL::Link')->search({
+                'url' => $url,
+            });
+
+            if ($link->count){
+                #already submitted
+                $c->stash->{'error'} = $error_codes[7];
+            }
+
+        } elsif ($c->stash->{'location'} eq 'blog') {
+
+        } elsif ($c->stash->{'location'} eq 'picture') {
+
+        }
+    }
+}
+
+=head2 post
+post logic
+=cut
+sub post: PathPart('post') Chained('validate') Args(0){
+    my ( $self, $c ) = @_;
+
+    if ($c->stash->{'error'}){
+        $c->flash->{'message'} = $c->stash->{'error'};
+        $c->res->redirect('/submit/' . $c->stash->{'location'} . '/');
+        $c->detach();
+    }
 warn $c->stash->{'location'};
 warn Data::Dumper::Dumper($c->req->params);
 
