60caff91989225511e3b3f612352573de0d680c3
Mark Ellis <m@rkellis.com>
2009-12-22 21:56:46 +0000

    bringing live upto with server

--- code/page.php
+++ code/page.php
@@ -175,7 +175,7 @@ if (defined('MAGIC')) {
 	            .'<meta name="keywords" content="news community comments english lulz lol social lulzhq fun jokes '
 	            .'videos pictures share sharing lol lolz funny humour humur"/> '
 	            .'<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> '
-	            .'<link rel="stylesheet" type="text/css" title="default" href="/css/default.css?2=28" /> '
+	            .'<link rel="stylesheet" type="text/css" title="default" href="/css/default.css?2=32" /> '
 	            .'<link rel="shortcut icon" href="/favicon.ico" /> '
 	            .$script
 	            .'</head> ';
@@ -199,7 +199,7 @@ if (defined('MAGIC')) {
                 
                 window.addEvent('domready', function() {
                     $$('.mibbit').addEvent('click', function(e) {
-                        popUpWindow("https://widget.mibbit.com/?server=irc.newnet.net%3A%2B9999&chatOutputShowTimes=true"
+                        popUpWindow("https://widget.mibbit.com/?server=irc.thisaintnews.com%3A%2B6697&chatOutputShowTimes=true"
                   	  +"&channel=%23thisaintnews&settings=8a8a5ac18a22e7eecd04026233c3df93&autoConnect=true"
                 	  +"&nick=<?= $user->getUsername() ?>", 720, 400);
                 	e.stop();
