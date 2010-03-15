f66158be5bcdd16140377a3c049fb31c319bc037
Mark Ellis <m@rkellis.com>
2010-03-15 00:27:30 +0000

    blogs do bbcode shit, and template fixed

--- lib/TAN/Schema/Result/Blog.pm
+++ lib/TAN/Schema/Result/Blog.pm
@@ -18,6 +18,7 @@ __PACKAGE__->add_columns(
     default_value => undef,
     is_nullable => 0,
     size => 16777215,
+    accessor => '_details',
   },
   "title",
   {
@@ -39,6 +40,29 @@ __PACKAGE__->utf8_columns(qw/details title description/);
 
 # Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-11-04 22:01:19
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:2yuJn4YdJ2YjHiMM992ceQ
+
+use Parse::HTML;
+use Parse::BBCode::TAN;
+use HTML::Scrubber::StripScripts;
+
+#initilise these here for performance reasons
+my $bbcode = new Parse::BBCode::TAN;
+
+my $hss = HTML::Scrubber::StripScripts->new(
+  Allow_src      => 1,
+  Allow_href     => 1,
+);
+
+my $p = new Parse::HTML;
+
+sub details{
+    my ( $row ) = @_;
+
+    my $blog = $p->parse( $bbcode->render( $hss->scrub( $row->_details ) ) );
+
+#do some caching shit here...
+    return $blog;
+}
 
 __PACKAGE__->belongs_to(
   "object",
