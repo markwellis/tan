571a1c8a9e7adfa82502bd34ba38946d60294d5d
Mark Ellis <m@rkellis.com>
2010-04-19 21:28:36 +0000

    added picture index

---
+++ root/static/themes/classic-light/js/picture-index.js
@@ -0,0 +1,40 @@
+window.addEvent('domready', function() {
+    $$('.container').fade('out');
+
+    $$('.TAN-inside li').addEvents({
+        'mouseenter': function(e){
+            e.stop();
+            var container = this.getElement('.container');
+
+            if ($defined(container)){
+                var image_link = this.getElement('.picture_thumb');
+                container.position({
+                    'relativeTo': this,
+                    'offset': {
+                        'x': -180,
+                        'y': -200 
+                    }
+                });
+                
+                var fade = function fade(){
+                    this.fade('in');
+                    this.removeClass('hidden');
+                }
+
+                this.store('delay', fade.delay(300, container));
+            }
+        },
+        'mouseleave': function(e){
+            e.stop();
+            var container = this.getElement('.container');
+
+            if ($defined(container)){
+                container.fade('toggle');
+                container.addClass('hidden');
+
+                $clear(this.retrieve('delay'));
+                this.eliminate('delay');
+            }
+        }
+    });
+});
