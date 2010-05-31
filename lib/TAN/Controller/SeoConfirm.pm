55c674ee42b213519dce55bd8a49c6ab46904956
Mark Ellis <m@rkellis.com>
2010-05-31 10:18:42 +0000

    #135 - seo confirm stuff

---
+++ lib/TAN/Controller/SeoConfirm.pm
@@ -0,0 +1,79 @@
+package TAN::Controller::SeoConfirm;
+use strict;
+use warnings;
+
+use parent 'Catalyst::Controller';
+
+=head1 NAME
+
+TAN::Controller::SeoConfirm
+
+=head1 DESCRIPTION
+
+SeoConfirm Controller
+
+=head1 EXAMPLE
+
+I</googledc796c4dad406173.html>
+I</y_key_242ef28969a04b9c.html>
+
+=over
+
+search engine confirm keys
+
+=back
+
+=head1 METHODS
+
+=cut
+
+=head2 google: Path('/googledc796c4dad406173.html')
+
+B<@args = undef>
+
+=over
+
+google confirm key
+
+=back
+
+=cut
+sub google: Path('/googledc796c4dad406173.html'){
+    my ( $self, $c ) = @_;
+
+    #google just check if the path exists
+    $c->res->output(1);
+    $c->detach();
+}
+
+=head2 yahoo: Path('/googledc796c4dad406173.html')
+
+B<@args = undef>
+
+=over
+
+yahoo confirm key
+
+=back
+
+=cut
+sub yahoo: Path('/y_key_242ef28969a04b9c.html'){
+    my ( $self, $c ) = @_;
+
+    #yahoo needs this key
+    $c->res->output('adf14f5d24cef99f');
+    $c->detach();
+}
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
