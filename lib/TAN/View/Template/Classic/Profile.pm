6b695148283f9f2082a44508b8e1cca3176f90fa
Mark Ellis <m@rkellis.com>
2010-10-17 20:16:22 +0000

    #210 - user index page

---
+++ lib/TAN/View/Template/Classic/Profile.pm
@@ -0,0 +1,24 @@
+package TAN::View::Template::Classic::Profile;
+use base 'Catalyst::View::Perl::Template';
+
+sub process{
+    my ( $self, $c ) = @_;
+
+    push(@{$c->stash->{'css_includes'}}, 'profile');
+
+    my $out = '<ul class="TAN-inside">';
+
+    foreach my $user ( $c->stash->{'users'}->all ){
+        my $username = $c->view->html($user->username);
+        $out .= qq\
+            <li class="TAN-profile-avatar">
+                <a href="${username}/" title="${username}">
+                    <img class="TAN-news-avatar" src="@{[ $user->avatar($c) ]}" alt="${username}"/>
+                </a>
+            </li>\;
+    }
+    $out .= '</ul>';
+    return $out;
+}
+
+1;
