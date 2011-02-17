7fa56ec78ba1e926c45519cf39d5e998684f5527
Mark Ellis <m@rkellis.com>
2011-02-17 00:10:10 +0000

    #267 - project wonderful ads load after page

--- lib/TAN/View/Template/Classic/Lib/Ad.pm
+++ lib/TAN/View/Template/Classic/Lib/Ad.pm
@@ -31,16 +31,30 @@ sub process{
     my $out = qq\<div class="TAN-${position}-ad">\;
 
     if ( !$c->stash->{'no_ads'} ){
-        $out .= qq\
+        $out .= qq#
         <script type="text/javascript">//<![CDATA[
             var pw_d=document;
             pw_d.projectwonderful_adbox_id = "${ad_slot}";
             pw_d.projectwonderful_adbox_type = "${ad_type}";
             pw_d.projectwonderful_foreground_color = "";
             pw_d.projectwonderful_background_color = "";
+
+            var timestamp = new Date().getTime();
+            var url = "http://www.projectwonderful.com/nojs.php?id=${ad_slot}&type=${ad_type}&t=" + timestamp;
+            var img = Asset.image( url, {
+                "onload": function( img ){
+                    var a = new Element( 'a', { 
+                        "href" : "http://www.projectwonderful.com/out_nojs.php?r=0&c=0&id=${ad_slot}&type=${ad_type}",
+                        "target": "_blank",
+                        "rel": "nofollow"
+                    } );
+
+                    a.grab( img );
+                    \$\$('.TAN-${position}-ad').grab( a );
+                }
+            } );
             //]]>
-        </script>
-        <script type="text/javascript" src="http://www.projectwonderful.com/ad_display.js"></script>\;
+        </script>#;
     }
 
     $out .= '</div>';
