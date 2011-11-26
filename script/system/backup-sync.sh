c0b7ec4ed0cf29aa4a5f192b58d95fc9d5f7d63e
Mark Ellis <m@rkellis.com>
2011-11-26 21:07:05 +0000

    re #356 - backup scripts

--- script/system/backup-sync.sh
+++ script/system/backup-sync.sh
@@ -3,7 +3,7 @@ SERVER=thisaintnews.pl
 
 SOURCE=/mnt/stuff
 DESTINATION=/homez.365/thisaint/www/backups/tan
-BACKUP_DIRS="TAN/user trac svn backups"
+BACKUP_DIRS="TAN/user trac svn backups db_backups"
 
 LOCK_FILE=/tmp/backup-sync
 
---
+++ script/system/system-backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OUTPUT_DIR="/mnt/stuff/backups"
+OUTPUT_FILENAME="${OUTPUT_DIR}/$(date --rfc-3339=date).tar.xz"
+BIND_MOUNTPOINT="/mnt/backup"
+
+function debug(){
+    if [ $DEBUG ]; then
+        echo -e "\e[01;31m$@\e[00m"
+    fi
+}
+
+debug "bind mounting /"
+mkdir -p $BIND_MOUNTPOINT
+mount -o bind / $BIND_MOUNTPOINT
+
+debug "creating tarball"
+cd $BIND_MOUNTPOINT
+tar -cJpf $OUTPUT_FILENAME . --exclude "mnt/*" --exclude "tmp/*" --exclude "usr/tmp/*" --exclude "var/tmp/*" --exclude "var/cache/apt/archives/*" --exclude "var/lib/varnish"
+
+debug "creating sha512sum"
+sha512sum -b $OUTPUT_FILENAME > $OUTPUT_FILENAME.sha512sum
+
+debug "unmounting bind mount"
+umount $BIND_MOUNTPOINT
