1a158b89e9899d14e60ea9784e4e00399e0bf51d
Mark Ellis <m@rkellis.com>
2011-11-26 22:17:57 +0000

    version bump

--- lib/TAN.pm
+++ lib/TAN.pm
@@ -5,7 +5,7 @@ use namespace::autoclean;
 use Catalyst::Runtime 5.80;
 use Data::Dumper; #used in 500 error email
 
-our $VERSION = 3.2.8;
+our $VERSION = 3.2.9;
 
 use Catalyst qw/
     ConfigLoader
