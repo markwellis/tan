19ab53a9213457407fe9ef94efc103bc512a9f03
Mark Ellis <m@rkellis.com>
2012-01-17 03:11:08 +0000

    re #276 - create search index script updated for objects

--- script/site/create_search_index.pl
+++ script/site/create_search_index.pl
@@ -2,39 +2,74 @@
 use strict;
 use warnings;
 
-use KinoSearchX::Simple;
+use LucyX::Simple;
 
 use TAN::Model::MySQL;
 use Term::ProgressBar;
 
-my $index_path = '/mnt/stuff/TAN/search_index';
+use HTML::TreeBuilder;
+use HTML::FormatText;
 
-my $searcher = KinoSearchX::Simple->new({
+sub strip_tags{
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse(shift);
+    $tree->eof;
+
+    my $text;
+    if ($tree) {
+        $text = $tree->as_text();
+        $tree = $tree->delete;
+    }
+
+    return $text;
+}
+
+my $searcher = LucyX::Simple->new({
     'index_path' => '/mnt/stuff/TAN/search_index',
     'schema' => [
         {
+            'name' => 'id',
+            'type' => 'string',
+        },{
             'name' => 'title', 
             'boost' => 3,
+            'stored' => 0,
         },{
             'name' => 'description',
+            'stored' => 0,
         },{
-            'name' => 'id',
+            'name' => 'content',
+            'stored' => 0,
         },{
             'name' => 'type',
+            'stored' => 0,
         },{
             'name' => 'nsfw',
+            'stored' => 0,
+        },{
+            'name' => 'date',
+            'type' => 'int32',
+            'indexed' => 0,
+            'stored' => 0,
+            'sortable' => 1,
+        },{
+            'name' => 'username',
+            'stored' => 0,
+        },{
+            'name' => 'tag',
+            'stored' => 0,
         },
     ],
-    'search_fields' => ['title', 'description'],
+    'search_fields' => ['title', 'description', 'tag'],
     'search_boolop' => 'AND',
 });
 
 my $db = new TAN::Model::MySQL;
 
 my $objects = $db->resultset('Object')->search({
-        'type' => ['link', 'blog', 'picture', 'poll', 'video'],
+        'type' =>[qw/link blog picture poll video forum/],
     },{
-    'prefetch' => ['link', 'blog', 'picture', 'poll', 'video'],
+    'prefetch' => ['link', 'blog', 'picture', {'poll' => 'answers'}, 'video', 'forum', 'user'],
 });
 
 my $count = $objects->count;
@@ -45,22 +80,36 @@ my $progress = Term::ProgressBar->new({
 });
 $progress->minor(0);
 
-#create index
-$searcher->_indexer;
-
 while (my $object = $objects->next){
     my $type = $object->type;
 
     if ( defined($object->$type) ){
-        $searcher->create({
+        my $create = {
             'id' => $object->id,
             'type' => $type,
             'nsfw' => $object->nsfw,
             'title' => $object->$type->title,
             'description' => $object->$type->description,
-        });
+            'date' => $object->_created->epoch,
+            'username' => $object->user->username,
+            'tag' =>  join( ' ', map( $_->tag, $object->tags->all ) ),
+        };
+
+        if ( 
+            ( $type eq 'blog' )
+            || ( $type eq 'forum' )
+        ){
+            $create->{'content'} = strip_tags( $object->$type->_details );
+        }
+
+        if ( $type eq 'poll' ){
+            $create->{'content'} = join( ' ', map( $_->answer, $object->poll->answers->all ) );
+        }
+        $searcher->create( $create );
     }
     $progress->update( ++$loop );
 }
+
+#add in comments here
 
 $searcher->commit;
