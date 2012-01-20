caaea5fef40e577476e70b39ecf94a00b209e254
Mark Ellis <m@rkellis.com>
2012-01-20 02:53:34 +0000

    closes #369 - create index script doesnt add deleted items

--- script/site/create_search_index.pl
+++ script/site/create_search_index.pl
@@ -59,11 +59,12 @@ my $searcher = LucyX::Simple->new({
 my $db = new TAN::Model::MySQL;
 
 my $objects = $db->resultset('Object')->search({
+        'deleted' => 'N',
         'type' =>[qw/link blog picture poll video forum/],
     },{
     'prefetch' => ['link', 'blog', 'picture', {'poll' => 'answers'}, 'video', 'forum', 'user'],
 });
-my $comments = $db->resultset('Comments')->search({},{'prefetch' => [qw/user/],});
+my $comments = $db->resultset('Comments')->search({ 'deleted' => 'N' },{'prefetch' => [qw/user/],});
 
 my $count = $objects->count + $comments->count;
 my $loop = 0;
