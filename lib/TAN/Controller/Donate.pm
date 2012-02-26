70f9e515b30d740f26939baae3b8e5f81ac08f2d
Mark Ellis <m@rkellis.com>
2012-02-26 03:23:28 +0000

    re #374 - some of paypal model and ipn

---
+++ lib/TAN/Controller/Donate.pm
@@ -0,0 +1,28 @@
+package TAN::Controller::Donate;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+use Try::Tiny;
+
+sub validate: Local{
+    my ( $self, $c ) = @_;
+    
+    my $error = try{
+        $c->model('PayPal')->validate( $c->req->params );
+    };
+
+    die if $error;
+    if ( $c->req->param('mc_gross') != $c->config->{'donate'}->{'cost'} ){
+        die 'invalid amount';
+    }
+    if ( $c->req->param('mc_currency') != $c->config->{'donate'}->{'currency'} ){
+        die 'invalid currency';
+    }
+# process payment
+    $c->res->output('ok');
+    $c->detach;
+};
+
+__PACKAGE__->meta->make_immutable;
--- lib/TAN/Model/PayPal.pm
+++ lib/TAN/Model/PayPal.pm
@@ -4,6 +4,9 @@ use namespace::autoclean;
 
 extends 'Catalyst::Model';
 
+use LWPx::ParanoidAgent;
+use Exception::Simple;
+
 has 'email' => (
     'isa' => 'Str',
     'is' => 'ro',
@@ -34,5 +37,38 @@ has 'sandbox' => (
     'required' => 1,
     'default' => 1,
 );
+
+sub validate{
+    my ( $self, $params ) = @_;
+
+    my $ua = LWPx::ParanoidAgent->new;
+    $params->{'cmd'} = '_notify-validate';
+    my $url = 'https://www';
+    if ( $self->sandbox ){
+        $url .= '.sandbox';
+    }
+    $url .= '.paypal.com/cgi-bin/webscr';
+    my $res = $ua->post( $url, $params );
+    if ( $res->is_error ){
+        Exception::Simple->throw('http error');
+    } elsif ( $res->content eq 'VERIFIED' ){
+        if ( $params->{'payment_status'} ne 'Completed' ){
+# check that $txn_id has not been previously processed
+            Exception::Simple->throw('payment not completed');
+        }
+        if ( $params->{'receiver_email'} ne $self->email ){
+            Exception::Simple->throw('not our receiver_email');
+        }
+        return;
+    } elsif ($res->content eq 'INVALID') {
+        Exception::Simple->throw("invalid");
+    }
+    
+    Exception::Simple->throw("unkown error");
+}
+
+sub make_payment{
+    my ( $self ) = @_;
+}
 
 __PACKAGE__->meta->make_immutable;
