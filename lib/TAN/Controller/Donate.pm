41c1264ddc3cce04122f65d37d44c9c10e29828d
Mark Ellis <m@rkellis.com>
2012-03-05 09:03:04 +0000

    re #374 - paypal stuff kinda working

--- lib/TAN/Controller/Donate.pm
+++ lib/TAN/Controller/Donate.pm
@@ -8,6 +8,12 @@ use Try::Tiny;
 
 sub index: Private{
     my ( $self, $c ) = @_;
+    
+    if (!$c->user_exists){
+        $c->flash->{'message'} = 'Please login';
+        $c->res->redirect('/login/', 303);
+        $c->detach();
+    }
 
 #load in things here etc
 # load this months used numbers, put i hash like { number => row }
@@ -16,10 +22,30 @@ sub index: Private{
         'button' => $c->model('PayPal')->button( {
             'currency_code' => $c->config->{'donate'}->{'currency'},
             'amount' => $c->config->{'donate'}->{'cost'},
-            'notify_url' => $c->uri_for('/donate/validate'),
+            'notify_url' => $c->uri_for('/donate/validate/'),
+            'return' => $c->uri_for('/donate/thankyou/'),
+            'cancel_return' => $c->uri_for('/donate/canceled/'),
+            'no_shipping' => 1,
+            'no_note' => 1,
         } ),
         'template' => 'donate.tt',
         'page_title' => 'Donate',
+    );
+}
+
+sub thankyou: Local{
+    my ( $self, $c ) = @_;
+
+    $c->stash(
+        'template' => 'donate/thankyou.tt',
+    );
+}
+
+sub canceled: Local{
+    my ( $self, $c ) = @_;
+
+    $c->stash(
+        'template' => 'donate/canceled.tt',
     );
 }
 
@@ -34,7 +60,7 @@ sub validate: Local{
     if ( $c->req->param('mc_gross') != $c->config->{'donate'}->{'cost'} ){
         die 'invalid amount';
     }
-    if ( $c->req->param('mc_currency') != $c->config->{'donate'}->{'currency'} ){
+    if ( $c->req->param('mc_currency') ne $c->config->{'donate'}->{'currency'} ){
         die 'invalid currency';
     }
 # process payment
