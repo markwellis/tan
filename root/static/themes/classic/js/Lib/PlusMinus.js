738c1a050406f207037ebb86aa840111ae638030
Mark Ellis <m@rkellis.com>
2013-01-18 20:00:35 +1100

    closes #449

--- root/static/themes/classic/js/Lib/PlusMinus.js
+++ root/static/themes/classic/js/Lib/PlusMinus.js
@@ -2,7 +2,7 @@ window.addEvent('domready', function() {
 	$$('.TAN-plusminus a').addEvent('click', function(e) {
         e.stop();
         
-        var plusminus = this.getParent().getParent().getParent();
+        var plusminus = this.getParent();
         new Request.JSON({
             'url': this.href,
             'noCache': true,
@@ -12,35 +12,35 @@ window.addEvent('domready', function() {
                     return;
                 }
 
-                var plus_selected = plusminus.getElement('.TAN-plus-arrow-selected');
+                var plus_selected = plusminus.getElement('.TAN-plus-selected');
                 if ( plus_selected ){
-                    if ( plus_selected.hasClass('TAN-plus-arrow-selected') ){
-                        var remove_plus_selected = new Fx.Morph( plus_selected, {
+                    if ( plus_selected.hasClass('TAN-plus-selected') ){
+                        plus_selected.set('morph', {
                             "link": "chain",
                             "onComplete": function(){
-                                plus_selected.removeClass('TAN-plus-arrow-selected');
+                                plus_selected.removeClass('TAN-plus-selected');
                             }
                         } );
-                        remove_plus_selected.start('.TAN-plus-arrow');
+                        plus_selected.morph('.TAN-plus');
                     }
                 }
                 
-                var minus_selected = plusminus.getElement('.TAN-minus-arrow-selected');
+                var minus_selected = plusminus.getElement('.TAN-minus-selected');
                 if ( minus_selected ){
-                    if ( minus_selected.hasClass('TAN-minus-arrow-selected') ){
-                        var remove_minus_selected = new Fx.Morph( minus_selected, {
+                    if ( minus_selected.hasClass('TAN-minus-selected') ){
+                        minus_selected.set('morph', {
                             "link": "chain",
                             "onComplete": function(){
-                                minus_selected.removeClass('TAN-minus-arrow-selected');
+                                minus_selected.removeClass('TAN-minus-selected');
                             }
                         } );
-                        remove_minus_selected.start('.TAN-minus-arrow');
+                        minus_selected.morph('.TAN-minus');
                     }
                 }
 
                 //add class to correct item
                 if ( json.created ){
-                    var classname = 'TAN-' + json.created + '-arrow';
+                    var classname = 'TAN-' + json.created;
                     var el = plusminus.getElement('.' + classname);
                     el.set('morph', {
                         "link": "chain",
@@ -65,8 +65,8 @@ window.addEvent('domready', function() {
                     } );
                 }
                 //update plus/minus/score count
-                plusminus.getElement('.TAN-plus').set( 'html', json.plus );
-                plusminus.getElement('.TAN-minus').set( 'html', json.minus );
+                plusminus.getElement('.TAN-object-plus').set( 'html', json.plus );
+                plusminus.getElement('.TAN-object-minus').set( 'html', json.minus );
                 plusminus.getElement('.TAN-object-score').set( 'html', json.score );
             }
         }).get({
