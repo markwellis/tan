11ba47b2c67aa3ce749d07d7ca4950df3ce83114
Mark Ellis <m@rkellis.com>
2013-01-25 22:33:18 +1100

    re #420 - css fixes

--- assets/art/design/js/template/lib/object/link.js
+++ assets/art/design/js/template/lib/object/link.js
@@ -6,27 +6,30 @@
             'class': 'TAN-type-' + stash.type,
             'text': stash.title
         }),
-        new Element('a', {
-            'href': 'view/' + stash.type + '/' + stash.id
-        }).adopt(
-            (function(){
-                if ( stash.type === 'video' ){
-                    return new Element('div', {
-                        'class': 'TAN-video',
-                        'html': stash.embed
-                    });
-                } else {
-                    return new Element('img', {
+        (function(){
+            if ( stash.type === 'video' ){
+                return new Element('div', {
+                    'class': 'TAN-video',
+                    'html': stash.embed
+                });
+            } else {
+                return new Element('a', {
+                    'href': 'view/' + stash.type + '/' + stash.id
+                }).adopt(
+                    new Element('img', {
                         'class': 'TAN-object-image',
                         'src': stash.image,
                         'alt': ''
-                    });
-                }
-            })()
-        ),
-        new Element('p', {
-            'text': stash.description ? stash.description : ''
-        }),
+                    })
+                );
+            }
+            
+            if ( stash.description ){
+                return new Element('p', {
+                    'text': stash.description
+                })
+            }
+        })(),
         new Element('div', {
             'class': 'TAN-object-info-bar'
         }).adopt(
@@ -36,7 +39,7 @@
                 new Element('a', {
                     'class': 'TAN-object-user',
                     'href': 'profile/' + stash.username.toURI(),
-                    'text': ' ' + short_username 
+                    'text': short_username 
                 }).grab(
                     new Element('img', {
                         'src': 'images/avatar.gif',
@@ -66,7 +69,7 @@
             new Element('span').adopt(
                 new Element('a', {
                     'href': '#',
-                    'text': ' ' + stash.comments
+                    'text': stash.comments
                 }).grab(
                     new Element('img', {
                         'src': 'images/comment.png',
