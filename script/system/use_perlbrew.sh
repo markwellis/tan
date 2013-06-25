e28ad2749f8e6bda7f27d8ad026121b485d1d4d0
Mark Ellis <m@rkellis.com>
2013-06-25 11:29:40 +0100

    closes #524 - fixed dev not using custom PERL5LIB

--- script/system/use_perlbrew.sh
+++ script/system/use_perlbrew.sh
@@ -6,6 +6,6 @@ export SHELL=bash
 source /mnt/stuff/perl/etc/bashrc 
 
 if [ $(hostname) == 'tandev' ]; then 
-    export perl5lib="/mnt/stuff/perl_modules/data-validate-image/lib:/mnt/stuff/perl_modules/exception-simple/lib:/mnt/stuff/perl_modules/fetch-image/lib:/mnt/stuff/perl_modules/gearmanx-simple/lib:/mnt/stuff/perl_modules/html-video-embed/lib:/mnt/stuff/perl_modules/lucyx-simple/lib:$perl5lib"
+    export PERL5LIB="/mnt/stuff/perl_modules/data-validate-image/lib:/mnt/stuff/perl_modules/exception-simple/lib:/mnt/stuff/perl_modules/fetch-image/lib:/mnt/stuff/perl_modules/gearmanx-simple/lib:/mnt/stuff/perl_modules/html-video-embed/lib:/mnt/stuff/perl_modules/lucyx-simple/lib:$PERL5LIB"
     DEVELOPMENT=1
 fi
