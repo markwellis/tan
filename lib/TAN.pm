9d9ca21ec4118fc0200711836bd8faaf3006e711
Mark Ellis <m@rkellis.com>
2013-07-20 15:53:40 +0100

    version bump

--- lib/TAN.pm
+++ lib/TAN.pm
@@ -6,7 +6,7 @@ use Catalyst::Runtime 5.90;
 use Data::Dumper; #used in 500 error email
 use 5.014;
 
-our $VERSION = 5.002015;
+our $VERSION = 5.002016;
 $VERSION = eval $VERSION;
 
 use Catalyst qw/
