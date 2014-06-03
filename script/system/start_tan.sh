b93fd8e139ed960442ecd5a9e72cea57cac084b7
Mark Ellis <m@rkellis.com>
2014-06-03 19:35:38 +0100

    changes that were on live :/

--- script/system/start_tan.sh
+++ script/system/start_tan.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 source "$(dirname "$0")/use_perlbrew.sh"
 
-#config
-WORKERS=5
 LISTEN=localhost:8081
 
-if [ $DEVELOPMENT ]; then 
-    export CATALYST_DEBUG=1
-    export CATALYST_CONFIG_LOCAL_SUFFIX=devel
-    WORKERS=1
-fi;
-
-exec starman --workers $WORKERS --l $LISTEN /var/www/TAN/tan.psgi
+exec plackup -s Starlet -l $LISTEN --max-workers 40 --no-default-middleware /var/www/TAN/tan.psgi
