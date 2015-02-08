27aa0ec3657ed99932467fb90f2f05b0c5787e51
Mark Ellis <m@rkellis.com>
2015-02-08 16:39:14 +0000

    fixed carton command

--- script/system/start_tan_worker.sh
+++ script/system/start_tan_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "$(dirname "$0")/use_perlbrew.sh"
 
-exec carton -cpanfile /var/www/TAN/cpanfile exec perl /var/www/TAN/script/workers/$1/$1.pl
+exec carton exec -cpanfile /var/www/TAN/cpanfile perl /var/www/TAN/script/workers/$1/$1.pl
