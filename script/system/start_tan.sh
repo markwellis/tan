54ff439bc6b65e38d46446755f002638af6b2205
Mark Ellis <m@rkellis.com>
2015-02-08 16:36:44 +0000

    added cpanfile path to start tan and worker scripts

--- script/system/start_tan.sh
+++ script/system/start_tan.sh
@@ -3,4 +3,4 @@ source "$(dirname "$0")/use_perlbrew.sh"
 
 LISTEN=localhost:8081
 
-exec carton exec plackup -s Gazelle -l $LISTEN --max-workers 40 --no-default-middleware /var/www/TAN/tan.psgi
+exec carton -cpanfile /var/www/TAN/cpanfile exec plackup -E production -s Gazelle -l $LISTEN --max-workers 40 --no-default-middleware /var/www/TAN/tan.psgi
