f0ca712e6cab8234877a50094e2c1b7084502ab3
Mark Ellis <m@rkellis.com>
2018-09-12 08:32:47 +0100

    reworked registration controller to use try catch

--- lib/TAN/Controller/Login/Registration.pm
+++ lib/TAN/Controller/Login/Registration.pm
@@ -10,8 +10,6 @@ use Try::Tiny;
 sub index: Path Args(0){
     my ( $self, $c ) = @_;
 
-# do some security shit here, perhaps add a nonce
-
 #check recaptcha
     my $result = do {
         #meh
@@ -29,58 +27,47 @@ sub index: Path Args(0){
     my $username = $c->req->param("rusername");
     my $email = $c->req->param("remail");
 
-    my $error;
-    if ( !$result->{'is_valid'} ){
-    # recaptcha failed
-        $error = "Captcha words do not match";
-    } elsif ( !$password0  || !$password1 || !$email || !$username ){
-    #form incomplete
-        $error = 'Please complete the form';
-    } elsif ( !Data::Validate::Email::is_email($email) ){
-    #invalid email address
-        $error = 'Not an valid email address';
-    } elsif ( $password0 ne $password1 ){
-    #passwords dont match
-        $error = 'Passwords do not match';
-    } elsif ( $username =~ m/\W+/g ){
-    #username cantains invalid chars
-        $error = 'Username can only contain letters or numbers';
-    } elsif ( $c->model('DB::User')->username_exists($username) ) {
-    #username exists
-        $error = 'Username already exists';
-    } elsif ( $c->model('DB::User')->email_exists($email) ) {
-    #email exists
-       $error = 'Email address already exists';
-    }
-
-#return new user object or error
     my $new_user;
-    if ( !$error ){
-        $new_user = try{
-            $c->model('DB::User')->new_user($username, $password0, $email);
-        } catch {
-            $error = $_;
-            return undef;
-        };
+    try {
+        if ( !$result->{'is_valid'} ){
+        # recaptcha failed
+            die "Captcha words do not match";
+        }
+        if ( !$password0  || !$password1 || !$email || !$username ){
+        #form incomplete
+            die 'Please complete the form';
+        }
+        if ( !Data::Validate::Email::is_email($email) ){
+        #invalid email address
+            die 'Not an valid email address';
+        }
+        if ( $password0 ne $password1 ){
+        #passwords dont match
+            die 'Passwords do not match';
+        }
+        if ( $username =~ m/\W+/g ){
+        #username cantains invalid chars
+            die 'Username can only contain letters or numbers';
+        }
+        if ( $c->model('DB::User')->username_exists($username) ) {
+        #username exists
+            die 'Username already exists';
+        }
+        if ( $c->model('DB::User')->email_exists($email) ) {
+        #email exists
+           die 'Email address already exists';
+        }
+        $new_user = eval {$c->model('DB::User')->new_user($username, $password0, $email);}
+        if $@ die "problem registering";
     }
-
-    if ( $error ){
+    catch {
         $c->flash->{'username'} = $username;
         $c->flash->{'email'} = $email;
 
         $c->flash->{'message'} = $error;
         $c->res->redirect( '/login/', 303 );
-        $c->detach();
-    }
-
-    if ( !$new_user ){
-        $c->flash->{'username'} = $c->req->param('rusername');
-        $c->flash->{'email'} = $c->req->param('remail');
-
-        $c->flash->{'message'} = 'Something done fucked up';
-        $c->res->redirect( '/login', 303 );
-        $c->detach();
-    }
+        $c->detach;
+    };
 
     #get new users token
 # mail token to user
